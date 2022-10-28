7a1b802f98036fd9e9475e5c9eaa1c52876ffa64
hmhtom <hmhtom@gmail.com>
2022-10-28 01:31:05 -0400

    added comment functionality

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+
+router.post("/", async (req, res) => {
+  if (req.session.logged_in) {
+    try {
+      const commentData = await Comment.create({
+        content: req.body.content,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id,
+      });
+      res.status(200).json(commentData);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+});
+
+module.exports = router;
